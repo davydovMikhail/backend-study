a7cb5c6fbfaeaf7fe06d44fcebd75fa91aeb06e0
davydovMikhail <davydov27977@gmail.com>
2022-08-22 08:58:54 +0700

    stage roles

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 
 async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
 
+    const config = new DocumentBuilder()
+        .setTitle('Проект один')
+        .setDescription('Документация первого проекта')
+        .setVersion('1.0.0')
+        .addTag('DM')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document) 
+
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 start()
-// 24 minut
+//34 minut

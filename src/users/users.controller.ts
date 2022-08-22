9a8d5387804494668e9bcb590067cc96c8a7090e
davydovMikhail <davydov27977@gmail.com>
2022-08-22 17:59:08 +0700

    guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from './users.model';
 import { UsersService } from "./users.service";
@@ -19,6 +22,8 @@ export class UsersController {
 
     @ApiOperation({summary: 'Получить всех пользователей'})
     @ApiResponse({status: 200, type: [User]})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.usersService.getAllUsers() ;
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -6,14 +6,18 @@ import { User } from './users.model';
 import { Role } from '../roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
     SequelizeModule.forFeature([User, Role, UserRoles]),
-    RolesModule
+    RolesModule,
+    forwardRef(() => AuthModule)
   ],
-  exports: [UsersService]
+  exports: [
+    UsersService
+  ]
 })
 export class UsersModule {}
